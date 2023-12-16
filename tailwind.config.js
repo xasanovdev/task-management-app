7c1c03d8876c867941b89acc722d2142402de369
Asranov Baxtinur <asranov0003@gmail.com>
2023-12-16 18:14:13 +0500

    feat: change variable colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#635fc7",
+        "primary-light": "#a7a4ff",
+        "primary-disabled": "#39395b",
+        grey: "828fa3",
+        danger: "ea5555",
+        "danger-light": "#ff9898",
+      },
+    },
   },
   plugins: [],
 };
